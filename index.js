74b86e6ff9fb4758d676fae9be7c6a140937279a
Amanda DiNoto <amanda.dinoto@gmail.com>
2024-05-17 15:19:17 -0400

    add db connection and routes and controllers for user table

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import express, { json } from 'express';
+const express = require('express');
+const bodyParser = require('body-parser')
+const db = require('./database')
 
 const app = express();
-app.use(json());
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running on PORT:${PORT}`);
 });
+
+app.get("/", async(req, res) => {
+  res.json({ info: 'Node.js, Express, Postgres API'})
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.get("/status", (req, res) => {
   const status = {
