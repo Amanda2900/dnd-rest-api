3b6e08ea606451879956d9e6c1ecdb503b6f43b4
Amanda DiNoto <amanda.dinoto@gmail.com>
2024-06-20 17:22:20 -0400

    logging middleware

---
+++ middleware/logger.js
@@ -0,0 +1,29 @@
+let callReceived;
+
+
+const logging = (req, res, next) => {
+  callReceived = new Date();
+  const today = new Date();
+  console.log('----------------')
+
+  console.log('Date: ', today.toUTCString());
+  console.log('Method: ', req.method)
+  console.log('Original URL: ', req.originalUrl);
+  console.log('Params: ', req.params);
+  console.log('Query: ', req.query)
+  next()
+}
+
+const loggingTwo = (req, res, next) => {
+  console.log('Called TailLog')
+  const tailDate = new Date();
+  const timeElapsed = tailDate - callReceived;
+  console.log('Time Elapsed: ', timeElapsed)
+  next()
+}
+
+const weLovePancakes = (req, res, next) => {
+  console.log('I love pancake.')
+}
+
+export { logging, loggingTwo, weLovePancakes };
